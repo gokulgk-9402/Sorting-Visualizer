a5e2079e3054e1435eacaa20d52fc14c4784387b
gokulgk-9402 <gokulgk9402@gmail.com>
2022-07-06 16:46:11 +0530

    More optimized code for pygame

--- main.py
+++ main.py
@@ -36,13 +36,10 @@ pygame.display.set_caption("Sorting Visualizer")
 bubble_sort_surface = pygame.Surface((SURFACE_WIDTH, SURFACE_HEIGHT))
 insertion_sort_surface = pygame.Surface((SURFACE_WIDTH, SURFACE_HEIGHT))
 selection_sort_surface = pygame.Surface((SURFACE_WIDTH, SURFACE_HEIGHT))
+merge_sort_surface = pygame.Surface((SURFACE_WIDTH, SURFACE_HEIGHT))
 
 list_of_numbers = []
 timings = {}
-
-bubble_done = False
-insertion_done = False
-selection_done = False
 
 def generate_numbers(n, minimum, maximum):
     numbers = []
@@ -52,29 +49,22 @@ def generate_numbers(n, minimum, maximum):
     return numbers
 
 def draw_static_items():
-    global bubble_done, insertion_done
     window.fill(WHITE)
     title = TITLE_FONT.render("Sorting Visualizer", True, BLACK)
     rectangle = (WIDTH//2 - title.get_width()//2, 40)
     window.blit(title, rectangle)
 
     bubble_title = FONT.render("Bubble Sort", True, BLACK)
-    window.blit(bubble_title, (15, 365))
-    if bubble_done:
-        time_text = TIME_FONT.render(f"  Time Taken: {round(timings['bubble'], 2)} seconds", True, BLACK)
-        window.blit(time_text, (500-time_text.get_width()-20, 372))
+    window.blit(bubble_title, (15, 365))        
 
     insertion_title = FONT.render("Insertion Sort", True, BLACK)
     window.blit(insertion_title, (515, 365))
-    if insertion_done:
-        time_text = TIME_FONT.render(f"  Time Taken: {round(timings['insertion'], 2)} seconds", True, BLACK)
-        window.blit(time_text, (1000-time_text.get_width()-20, 372))
+
+    merge_title = FONT.render("Merge Sort", True, BLACK)
+    window.blit(merge_title, (15, 665))
 
     selection_title = FONT.render("Selection Sort", True, BLACK)
     window.blit(selection_title, (15, 965))
-    if selection_done:
-        time_text = TIME_FONT.render(f"  Time Taken: {round(timings['selection'], 2)} seconds", True, BLACK)
-        window.blit(time_text, (500-time_text.get_width()-20, 972))
 
 def draw_bubble_surface(numbers, color_positions = {}):
     bubble_sort_surface.fill(WHITE)
@@ -131,7 +121,6 @@ def draw_selection_surface(numbers, color_positions = {}):
     window.blit(selection_sort_surface, (0,700))
 
 def bubble_sort(numbers):
-    global bubble_done
     bubble_start = time.time()
     for i in range(len(numbers)):
         for j in range(len(numbers) - i - 1):
@@ -139,16 +128,16 @@ def bubble_sort(numbers):
                 numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
                 draw_bubble_surface(numbers, {j:RED, j+1:GREEN})
                 pygame.display.flip()
-                time.sleep(0.02)
-    bubble_done = True
+                time.sleep(0.01)
 
     bubble_end = time.time()
     draw_bubble_surface(numbers)
-    pygame.display.flip()
     timings["bubble"] = bubble_end - bubble_start
+    time_text = TIME_FONT.render(f"  Time Taken: {round(timings['bubble'], 2)} seconds", True, BLACK)
+    window.blit(time_text, (500-time_text.get_width()-20, 372))
+    pygame.display.flip()
 
 def insertion_sort(numbers):
-    global insertion_done
     insertion_start = time.time()
     for i in range(1, len(numbers)):
         key = numbers[i]
@@ -158,31 +147,28 @@ def insertion_sort(numbers):
             numbers[j+1] = numbers[j]
             draw_insertion_surface(numbers, {j:RED, j+1:GREEN})
             pygame.display.flip()
-            time.sleep(0.02)
+            time.sleep(0.01)
             j -= 1
         
         if numbers[j+1] != key:
             numbers[j+1] = key
             draw_insertion_surface(numbers, {i:RED, j+1:GREEN})
             pygame.display.flip()
-            time.sleep(0.02)
-    insertion_done = True
+            time.sleep(0.01)
 
     insertion_end = time.time()
+    timings["insertion"] = insertion_end - insertion_start
+    time_text = TIME_FONT.render(f"  Time Taken: {round(timings['insertion'], 2)} seconds", True, BLACK)
+    window.blit(time_text, (1000-time_text.get_width()-20, 372))
     draw_insertion_surface(numbers)
     pygame.display.flip()
-    timings["insertion"] = insertion_end - insertion_start
-
 
 def selection_sort(numbers):
-    global selection_done
     selection_start = time.time()
     for i in range(len(numbers)):
         minimum = i
         for j in range(i+1, len(numbers)):
             if numbers[minimum] > numbers[j]:
-                # draw_selection_surface(numbers, {j:RED, minimum: GREEN})
-                # pygame.display.flip()
                 time.sleep(0.02)
                 minimum = j
 
@@ -192,27 +178,30 @@ def selection_sort(numbers):
             pygame.display.flip()
             time.sleep(0.02)
 
-    selection_done = True
-
     selection_end = time.time()
+    timings["selection"] = selection_end - selection_start
+    time_text = TIME_FONT.render(f"  Time Taken: {round(timings['selection'], 2)} seconds", True, BLACK)
+    window.blit(time_text, (500-time_text.get_width()-20, 972))
     draw_selection_surface(numbers)
     pygame.display.flip()
-    timings["selection"] = selection_end - selection_start
 
 list_of_numbers = generate_numbers(50, 0, 100)
 
 bubble_sort_numbers = list_of_numbers[:]
 insertion_sort_numbers = list_of_numbers[:]
+merge_sort_numbers = list_of_numbers[:]
 selection_sort_numbers = list_of_numbers[:]
 
 clock = pygame.time.Clock()
+draw_static_items()
+
+draw_bubble_surface(bubble_sort_numbers)
+draw_insertion_surface(insertion_sort_numbers)
+# draw_merge_surface(merge_sort_numbers)
+draw_selection_surface(selection_sort_numbers)
 
 while True:
     clock.tick(60)
-    draw_static_items()
-    draw_bubble_surface(bubble_sort_numbers)
-    draw_insertion_surface(insertion_sort_numbers)
-    draw_selection_surface(selection_sort_numbers)
 
     for event in pygame.event.get():
 
@@ -225,22 +214,33 @@ while True:
                     list_of_numbers = generate_numbers(50, 0, 100)
                     bubble_sort_numbers = list_of_numbers[:]
                     insertion_sort_numbers = list_of_numbers[:]
-                    bubble_done = False
-                    insertion_done = False
-
+                    merge_sort_numbers = list_of_numbers[:]
+                    selection_sort_numbers = list_of_numbers[:]
+                    
+                    draw_bubble_surface(bubble_sort_numbers)
+                    draw_insertion_surface(insertion_sort_numbers)
+                    # draw_merge_surface(merge_sort_numbers)
+                    draw_selection_surface(selection_sort_numbers)
                 
                 elif event.key == pygame.K_SPACE:
                     bubble_thread = threading.Thread(target=bubble_sort, args=(bubble_sort_numbers,))
                     insertion_thread = threading.Thread(target=insertion_sort, args=(insertion_sort_numbers,))
+                    # merge_thread = threading.Thread(target=merge_sort, args=(merge_sort_numbers, ))
                     selection_thread = threading.Thread(target=selection_sort, args=(selection_sort_numbers, ))
 
                     bubble_thread.start()
                     insertion_thread.start()
+                    # merge_thread.start()
                     selection_thread.start()
 
                     bubble_thread.join()
                     insertion_thread.join()
+                    # merge_thread.join()
                     selection_thread.join()
+                    # merge_sort(merge_sort_numbers)
+                    # selection_sort(selection_sort_numbers)
+                    # bubble_sort(bubble_sort_numbers)
+                    # insertion_sort(insertion_sort_numbers)
 
                     print(timings)
 
