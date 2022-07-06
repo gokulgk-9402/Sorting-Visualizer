a10f3724499a4f41ca89fd2dabb0b00aa0b68e33
gokulgk-9402 <gokulgk9402@gmail.com>
2022-07-06 17:01:49 +0530

    Placeholders for merge, quick, shell sort done

--- main.py
+++ main.py
@@ -37,6 +37,8 @@ bubble_sort_surface = pygame.Surface((SURFACE_WIDTH, SURFACE_HEIGHT))
 insertion_sort_surface = pygame.Surface((SURFACE_WIDTH, SURFACE_HEIGHT))
 selection_sort_surface = pygame.Surface((SURFACE_WIDTH, SURFACE_HEIGHT))
 merge_sort_surface = pygame.Surface((SURFACE_WIDTH, SURFACE_HEIGHT))
+quick_sort_surface = pygame.Surface((SURFACE_WIDTH, SURFACE_HEIGHT))
+shell_sort_surface = pygame.Surface((SURFACE_WIDTH, SURFACE_HEIGHT))
 
 list_of_numbers = []
 timings = {}
@@ -63,8 +65,14 @@ def draw_static_items():
     merge_title = FONT.render("Merge Sort", True, BLACK)
     window.blit(merge_title, (15, 665))
 
+    selection_title = FONT.render("Quick Sort", True, BLACK)
+    window.blit(selection_title, (515, 665))
+
     selection_title = FONT.render("Selection Sort", True, BLACK)
     window.blit(selection_title, (15, 965))
+
+    selection_title = FONT.render("Shell Sort", True, BLACK)
+    window.blit(selection_title, (515, 965))
 
 def draw_bubble_surface(numbers, color_positions = {}):
     bubble_sort_surface.fill(WHITE)
@@ -84,6 +92,24 @@ def draw_bubble_surface(numbers, color_positions = {}):
 
     window.blit(bubble_sort_surface, (0,100))
 
+def draw_merge_surface(numbers, color_positions = {}):
+    merge_sort_surface.fill(WHITE)
+
+    for i in range(len(numbers)):
+        x = i * BAR_WIDTH + SURFACE_PADDING
+        y = SURFACE_HEIGHT - (numbers[i]*BAR_HEIGHT) - SURFACE_PADDING
+
+        color = COLORS[i%3]
+
+        if i in color_positions:
+            color = color_positions[i]
+
+        height = numbers[i] * BAR_HEIGHT
+
+        pygame.draw.rect(merge_sort_surface, color, (x, y, BAR_WIDTH, height))
+
+    window.blit(merge_sort_surface, (0,400))
+
 def draw_insertion_surface(numbers, color_positions = {}):
     insertion_sort_surface.fill(WHITE)
     
@@ -101,6 +127,24 @@ def draw_insertion_surface(numbers, color_positions = {}):
         pygame.draw.rect(insertion_sort_surface, color, (x, y, BAR_WIDTH, height))
 
     window.blit(insertion_sort_surface, (500, 100))
+
+def draw_quick_surface(numbers, color_positions = {}):
+    quick_sort_surface.fill(WHITE)
+
+    for i in range(len(numbers)):
+        x = i * BAR_WIDTH + SURFACE_PADDING
+        y = SURFACE_HEIGHT - (numbers[i]*BAR_HEIGHT) - SURFACE_PADDING
+
+        color = COLORS[i%3]
+
+        if i in color_positions:
+            color = color_positions[i]
+
+        height = numbers[i] * BAR_HEIGHT
+
+        pygame.draw.rect(quick_sort_surface, color, (x, y, BAR_WIDTH, height))
+
+    window.blit(quick_sort_surface, (500,400))
 
 def draw_selection_surface(numbers, color_positions = {}):
     selection_sort_surface.fill(WHITE)
@@ -120,6 +164,24 @@ def draw_selection_surface(numbers, color_positions = {}):
 
     window.blit(selection_sort_surface, (0,700))
 
+def draw_shell_surface(numbers, color_positions = {}):
+    shell_sort_surface.fill(WHITE)
+
+    for i in range(len(numbers)):
+        x = i * BAR_WIDTH + SURFACE_PADDING
+        y = SURFACE_HEIGHT - (numbers[i]*BAR_HEIGHT) - SURFACE_PADDING
+
+        color = COLORS[i%3]
+
+        if i in color_positions:
+            color = color_positions[i]
+
+        height = numbers[i] * BAR_HEIGHT
+
+        pygame.draw.rect(shell_sort_surface, color, (x, y, BAR_WIDTH, height))
+
+    window.blit(shell_sort_surface, (500,700))
+
 def bubble_sort(numbers):
     bubble_start = time.time()
     for i in range(len(numbers)):
@@ -131,10 +193,10 @@ def bubble_sort(numbers):
                 time.sleep(0.01)
 
     bubble_end = time.time()
-    draw_bubble_surface(numbers)
     timings["bubble"] = bubble_end - bubble_start
     time_text = TIME_FONT.render(f"  Time Taken: {round(timings['bubble'], 2)} seconds", True, BLACK)
     window.blit(time_text, (500-time_text.get_width()-20, 372))
+    draw_bubble_surface(numbers)
     pygame.display.flip()
 
 def insertion_sort(numbers):
@@ -161,6 +223,57 @@ def insertion_sort(numbers):
     time_text = TIME_FONT.render(f"  Time Taken: {round(timings['insertion'], 2)} seconds", True, BLACK)
     window.blit(time_text, (1000-time_text.get_width()-20, 372))
     draw_insertion_surface(numbers)
+    pygame.display.flip()
+
+def merge_sort(numbers):
+    merge_start = time.time()
+    for i in range(len(numbers)):
+        for j in range(len(numbers) - i - 1):
+            if numbers[j] < numbers[j+1]:
+                numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+                draw_merge_surface(numbers, {j:RED, j+1:GREEN})
+                pygame.display.flip()
+                time.sleep(0.01)
+
+    merge_end = time.time()
+    timings["merge"] = merge_end - merge_start
+    time_text = TIME_FONT.render(f"  Time Taken: {round(timings['merge'], 2)} seconds", True, BLACK)
+    window.blit(time_text, (500-time_text.get_width()-20, 672))
+    draw_merge_surface(numbers)
+    pygame.display.flip()
+
+def quick_sort(numbers):
+    quick_start = time.time()
+    for i in range(len(numbers)):
+        for j in range(len(numbers) - i - 1):
+            if numbers[j] < numbers[j+1]:
+                numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+                draw_quick_surface(numbers, {j:RED, j+1:GREEN})
+                pygame.display.flip()
+                time.sleep(0.01)
+
+    quick_end = time.time()
+    timings["quick"] = quick_end - quick_start
+    time_text = TIME_FONT.render(f"  Time Taken: {round(timings['quick'], 2)} seconds", True, BLACK)
+    window.blit(time_text, (1000-time_text.get_width()-20, 672))
+    draw_quick_surface(numbers)
+    pygame.display.flip()
+
+def shell_sort(numbers):
+    shell_start = time.time()
+    for i in range(len(numbers)):
+        for j in range(len(numbers) - i - 1):
+            if numbers[j] < numbers[j+1]:
+                numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+                draw_shell_surface(numbers, {j:RED, j+1:GREEN})
+                pygame.display.flip()
+                time.sleep(0.01)
+
+    shell_end = time.time()
+    timings["shell"] = shell_end - shell_start
+    time_text = TIME_FONT.render(f"  Time Taken: {round(timings['shell'], 2)} seconds", True, BLACK)
+    window.blit(time_text, (1000-time_text.get_width()-20, 972))
+    draw_shell_surface(numbers)
     pygame.display.flip()
 
 def selection_sort(numbers):
@@ -191,14 +304,18 @@ bubble_sort_numbers = list_of_numbers[:]
 insertion_sort_numbers = list_of_numbers[:]
 merge_sort_numbers = list_of_numbers[:]
 selection_sort_numbers = list_of_numbers[:]
+quick_sort_numbers = list_of_numbers[:]
+shell_sort_numbers = list_of_numbers[:]
 
 clock = pygame.time.Clock()
 draw_static_items()
 
 draw_bubble_surface(bubble_sort_numbers)
 draw_insertion_surface(insertion_sort_numbers)
-# draw_merge_surface(merge_sort_numbers)
+draw_merge_surface(merge_sort_numbers)
 draw_selection_surface(selection_sort_numbers)
+draw_quick_surface(selection_sort_numbers)
+draw_shell_surface(selection_sort_numbers)
 
 while True:
     clock.tick(60)
@@ -216,29 +333,40 @@ while True:
                     insertion_sort_numbers = list_of_numbers[:]
                     merge_sort_numbers = list_of_numbers[:]
                     selection_sort_numbers = list_of_numbers[:]
+                    quick_sort_numbers = list_of_numbers[:]
+                    shell_sort_numbers = list_of_numbers[:]
                     
                     draw_bubble_surface(bubble_sort_numbers)
                     draw_insertion_surface(insertion_sort_numbers)
-                    # draw_merge_surface(merge_sort_numbers)
+                    draw_merge_surface(merge_sort_numbers)
                     draw_selection_surface(selection_sort_numbers)
+                    draw_quick_surface(selection_sort_numbers)
+                    draw_shell_surface(selection_sort_numbers)
                 
                 elif event.key == pygame.K_SPACE:
                     bubble_thread = threading.Thread(target=bubble_sort, args=(bubble_sort_numbers,))
                     insertion_thread = threading.Thread(target=insertion_sort, args=(insertion_sort_numbers,))
-                    # merge_thread = threading.Thread(target=merge_sort, args=(merge_sort_numbers, ))
+                    merge_thread = threading.Thread(target=merge_sort, args=(merge_sort_numbers, ))
                     selection_thread = threading.Thread(target=selection_sort, args=(selection_sort_numbers, ))
+                    shell_thread = threading.Thread(target=shell_sort, args=(shell_sort_numbers, ))
+                    quick_thread = threading.Thread(target=quick_sort, args=(quick_sort_numbers, ))
 
                     bubble_thread.start()
                     insertion_thread.start()
-                    # merge_thread.start()
+                    merge_thread.start()
                     selection_thread.start()
+                    shell_thread.start()
+                    quick_thread.start()
 
                     bubble_thread.join()
                     insertion_thread.join()
-                    # merge_thread.join()
+                    merge_thread.join()
                     selection_thread.join()
-                    # merge_sort(merge_sort_numbers)
-                    # selection_sort(selection_sort_numbers)
+                    shell_thread.join()
+                    quick_thread.join()
+
+                    # quick_sort(quick_sort_numbers)
+                    # shell_sort(shell_sort_numbers)
                     # bubble_sort(bubble_sort_numbers)
                     # insertion_sort(insertion_sort_numbers)
 
